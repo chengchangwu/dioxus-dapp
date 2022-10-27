3ba82125709ce7e7c67e856601a7d58165b1bb1c
Sirius Wu <ccwu660601@mapacode.tw>
2022-10-28 04:45:29 +0800

    Add tailwind.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Dioxus-dapp
 
 Scaffolding for a dapp build on Dioxus
+
+```
+npm install
+npx tailwindcss -i ./input.css -o ./output.css --watch
+trunk serve
+```
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use dioxus::prelude::*;
 use dioxus_dapp::{home::HomeView, Package, Wallet};
 
+static STYLES: &'static str = include_str!("../output.css");
+
 fn main() {
     dioxus::web::launch(app);
 }
@@ -9,6 +11,9 @@ fn app(cx: Scope) -> Element {
     let pkg = Package { version: "0.1.0" };
     let wallet = Wallet { balance: 0 };
     cx.render(rsx! {
+        style {
+            vec![STYLES]
+        }
         HomeView{
             pkg: pkg,
             wallet: wallet,
