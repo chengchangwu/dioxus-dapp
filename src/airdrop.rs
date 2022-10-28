10b9909d46b5b36414e49bea60d325f33bd1b40f
Sirius Wu <ccwu660601@mapacode.tw>
2022-10-28 08:20:43 +0800

    Implement UI for RequestAirdrop.

--- src/airdrop.rs
+++ src/airdrop.rs
@@ -1,10 +1,17 @@
 use dioxus::prelude::*;
 
-#[derive(PartialEq, Props)]
-pub struct RequestAirdropProps {}
-
 #[allow(non_snake_case)]
-pub fn RequestAirdrop(_cx: Scope<RequestAirdropProps>) -> Element {
-    // TODO
-    None
+pub fn RequestAirdrop(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            button {
+                class: "px-8 m-2 btn animate-pulse bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ...",
+                // TODO
+                onclick: move |_| {},
+                span {
+                    "Airdrop 1"
+                }
+            }
+        }
+    })
 }
