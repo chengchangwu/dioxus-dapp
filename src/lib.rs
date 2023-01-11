f023bc22bee5426203b84fc69d41d24463102702
Sirius Wu <ccwu660601@mapacode.tw>
2023-01-12 01:19:19 +0800

    Upgrade dioxus to 0.3.1.

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,25 @@
-use dioxus::prelude::*;
-use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
-
 pub mod components;
 pub mod views;
 pub mod wallets;
 
-use components::{app_bar::AppBar, footer::Footer};
-use views::home::HomeView;
+// #[wasm_bindgen(start)]
+// pub fn bootstrap() -> Result<(), JsValue> {
+//     // Use `web_sys`'s global `window` function to get a handle on the global
+//     // window object.
+//     let window = web_sys::window().expect("no global `window` exists");
+//     let document = window.document().expect("should have a document on window");
+//     let body = document.body().expect("document should have a body");
 
-#[wasm_bindgen(start)]
-pub fn bootstrap() -> Result<(), JsValue> {
-    // Use `web_sys`'s global `window` function to get a handle on the global
-    // window object.
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
+//     // Manufacture the element we're gonna append
+//     let val = document.create_element("p")?;
+//     val.set_inner_html("Hello from Rust!");
 
-    // Manufacture the element we're gonna append
-    let val = document.create_element("p")?;
-    val.set_inner_html("Hello from Rust!");
+//     body.append_child(&val)?;
 
-    body.append_child(&val)?;
+//     // TODO: execute main.
 
-    // TODO: execute main.
-
-    Ok(())
-}
+//     Ok(())
+// }
 
 #[derive(PartialEq)]
 pub struct Package<'a> {
@@ -35,42 +29,4 @@ pub struct Package<'a> {
 #[derive(PartialEq)]
 pub struct Wallet {
     pub balance: usize,
-}
-
-static STYLES: &'static str = include_str!("../output.css");
-
-#[wasm_bindgen]
-pub fn main() {
-    wasm_logger::init(wasm_logger::Config::default());
-    let window = wallets::window().unwrap();
-    let solana = window.solana();
-    log::info!("Solana {solana:?}");
-    if let Some(s) = solana {
-        log::info!(
-            "solana connected {}, is_phantom {}, is_brave_wallet {}",
-            s.is_connected(),
-            s.is_phantom(),
-            s.is_brave_wallet(),
-        );
-    }
-    dioxus::web::launch(app);
-}
-
-fn app(cx: Scope) -> Element {
-    let pkg = Package { version: "0.1.0" };
-    let wallet = Wallet { balance: 0 };
-    cx.render(rsx! {
-        style {
-            vec![STYLES]
-        }
-        div {
-            class: "flex flex-col h-screen",
-            AppBar {}
-            HomeView {
-                pkg: pkg,
-                wallet: wallet,
-            }
-            Footer {}
-        }
-    })
 }
---
+++ src/main.rs
@@ -0,0 +1,46 @@
+#![allow(non_snake_case)]
+// import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
+use dioxus::prelude::*;
+use dioxus_dapp::{
+    components::{app_bar::AppBar, footer::Footer},
+    views::home::HomeView,
+    Package, Wallet,
+};
+
+static STYLES: &'static str = include_str!("../output.css");
+
+fn main() {
+    wasm_logger::init(wasm_logger::Config::default());
+    // let window = wallets::window().unwrap();
+    // let solana = window.solana();
+    // log::info!("Solana {solana:?}");
+    // if let Some(s) = solana {
+    //     log::info!(
+    //         "solana connected {}, is_phantom {}, is_brave_wallet {}",
+    //         s.is_connected(),
+    //         s.is_phantom(),
+    //         s.is_brave_wallet(),
+    //     );
+    // }
+    dioxus_web::launch(App);
+}
+
+// create a component that renders a div with the text "Hello, world!"
+fn App(cx: Scope) -> Element {
+    let pkg = Package { version: "0.1.0" };
+    let wallet = Wallet { balance: 0 };
+    cx.render(rsx! {
+        style {
+            STYLES
+        }
+        div {
+            class: "flex flex-col h-screen",
+            // AppBar {}
+            HomeView {
+                pkg: pkg,
+                wallet: wallet,
+            }
+            Footer {}
+        }
+    })
+}
