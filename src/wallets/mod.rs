be4b732ffb1415c418a3c2eb61075c76118e98cc
Sirius Wu <ccwu660601@mapacode.tw>
2023-01-13 22:12:12 +0800

    Add bindings to local storage.

--- src/wallets/mod.rs
+++ src/wallets/mod.rs
@@ -1,6 +1,6 @@
 use js_sys::Object;
 use wasm_bindgen::prelude::*;
-use web_sys::EventTarget;
+use web_sys::{EventTarget, Storage};
 
 pub mod adapters;
 pub mod hooks;
@@ -10,9 +10,23 @@ pub mod ui;
 extern "C" {
     #[wasm_bindgen (extends = EventTarget, extends = Object, js_name = Window, typescript_type = "Window")]
     #[derive(Debug, Clone, PartialEq, Eq)]
+    #[doc = "The `Window` class."]
+    #[doc = ""]
+    #[doc = "[MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/API/Window)"]
+    #[doc = ""]
+    #[doc = "*Declared here instead of using web-sys' verion of `Window` in order to add our own API.*"]
     pub type Window;
     #[wasm_bindgen (structural, method, getter, js_class = "Window", js_name = window)]
+    #[doc = "Getter for the `window` field of this object."]
+    #[doc = ""]
+    #[doc = "[MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/API/Window/window)"]
     pub fn window(this: &Window) -> Window;
+    # [wasm_bindgen (structural , catch , method , getter , js_class = "Window" , js_name = localStorage)]
+    #[doc = "Getter for the `localStorage` field of this object."]
+    #[doc = ""]
+    #[doc = "[MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage)"]
+    pub fn local_storage(this: &Window) -> Result<Option<Storage>, JsValue>;
+
     #[derive(Debug, Clone, PartialEq)]
     pub type Solana;
     #[wasm_bindgen (structural, method, getter, js_class = "Window", js_name = solana)]
