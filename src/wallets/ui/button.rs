d20289170e7650440e69545521d878c5ba7281dd
Sirius Wu <ccwu660601@mapacode.tw>
2023-01-18 17:04:10 +0800

    Handle start and end icons.

--- src/wallets/ui/button.rs
+++ src/wallets/ui/button.rs
@@ -2,15 +2,14 @@ use dioxus::prelude::*;
 
 #[derive(Props)]
 pub struct ButtonProps<'a> {
-    #[props(default = "")]
+    #[props(default)]
     class: &'a str,
-    #[props(default = false)]
+    #[props(default)]
     disabled: bool,
-    #[props(default = None)]
-    end_icon: Element<'a>,
+    #[props(optional)]
+    end_icon: Option<Element<'a>>,
     // onClick?: (e: MouseEvent<HTMLButtonElement>) => void;
-    #[props(default = None)]
-    start_icon: Element<'a>,
+    start_icon: Option<Element<'a>>,
     // style?: CSSProperties;
     // tabIndex?: number;
     children: Element<'a>,
@@ -18,6 +17,38 @@ pub struct ButtonProps<'a> {
 
 #[allow(non_snake_case)]
 pub fn Button<'a>(cx: Scope<'a, ButtonProps<'a>>) -> Element<'a> {
+    let start_icon = match &cx.props.start_icon {
+        Some(Some(node)) => {
+            rsx! {
+                i {
+                    class: "wallet-adapter-button-start-icon",
+                    node
+                }
+            }
+        }
+        Some(None) => {
+            rsx! { Option::<VNode>::None }
+        }
+        None => {
+            rsx! { Option::<VNode>::None }
+        }
+    };
+    let end_icon = match &cx.props.end_icon {
+        Some(Some(node)) => {
+            rsx! {
+                i {
+                    class: "wallet-adapter-button-end-icon",
+                    node
+                }
+            }
+        }
+        Some(None) => {
+            rsx! { Option::<VNode>::None }
+        }
+        None => {
+            rsx! { Option::<VNode>::None }
+        }
+    };
     cx.render(rsx! {
         button {
             class: "wallet-adapter-button {cx.props.class}",
@@ -26,20 +57,9 @@ pub fn Button<'a>(cx: Scope<'a, ButtonProps<'a>>) -> Element<'a> {
             // onClick={props.onClick}
             // tabIndex={props.tabIndex || 0}
             r#type: "button",
-            // TODO
-            // &cx.props.start_icon.map(|icon| rsx!{
-            //     i {
-            //         class: "wallet-adapter-button-start-icon",
-            //         icon
-            //     }
-            // })
-            &cx.props.children
-            // &cx.props.end_icon.map(|icon| rsx!{
-            //     i {
-            //         class: "wallet-adapter-button-end-icon",
-            //         icon
-            //     }
-            // })
+            start_icon,
+            &cx.props.children,
+            end_icon
         }
     })
 }
--- src/wallets/ui/wallet_modal_button.rs
+++ src/wallets/ui/wallet_modal_button.rs
@@ -4,6 +4,10 @@ use crate::wallets::ui::Button;
 
 #[derive(Props)]
 pub struct WalletModalButtonProps<'a> {
+    #[props(optional)]
+    start_icon: Option<Element<'a>>,
+    #[props(optional)]
+    end_icon: Option<Element<'a>>,
     children: Element<'a>,
 }
 
@@ -11,6 +15,8 @@ pub struct WalletModalButtonProps<'a> {
 pub fn WalletModalButton<'a>(cx: Scope<'a, WalletModalButtonProps<'a>>) -> Element<'a> {
     cx.render(rsx! {
         Button {
+            start_icon: cx.props.start_icon.as_ref().map(|icon| icon.clone()).unwrap_or(None),
+            end_icon: cx.props.end_icon.as_ref().map(|icon| icon.clone()).unwrap_or(None),
             &cx.props.children
         }
     })
